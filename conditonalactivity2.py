13dd0593852f9ce684594c5e1524d5430fc01f17
kahlinixon <144710357+kahlinixon@users.noreply.github.com>
2023-09-29 16:25:28 +0000

    Conditionalactivity2.py

--- conditonalactivity2.py
+++ conditonalactivity2.py
@@ -8,6 +8,17 @@
 # "class is still in session."
 # your function should also alow the user to put in the time. The time should be 
 # formatted as a float. 
+def  classTime():
+    ApClassOver=11:30('class is over') 
+if 
+        classTime=11:30
+    print('Time to go class is over')
+else:
+    print('class is not over')
+
+    # 11:30=intro Over
+
+    #print("")
 
 # 2. Write a function that uses a conditional statement. 
 # your function should determine what type a pet a user has depeding on the data provided by the user
@@ -16,9 +27,24 @@
 # IF the user types in "meow", the function should return a message saying that it is a cat.
 # ELSE, if it is none of the animal sounds the function should return a message saying it doesn't 
 # know what the animal is. 
-
+def petType():
+     userTyped=woof("pet type is dog")
+if:
+    userType=woof
+    print('pet type is dog')
+else
+     print('pet type is cat')
 # 3. Write a function that will take in a user name and height as parameters. 
 # Your function should evaluate and determine if the user is tall in enough to get on a roller coster.
 # IF the user is over 5.5, the function should return a custom message saying the user's name
 # and a message "welcome please buckle up".
-# ELSE if they they are not, return a message apologizing to the user saying they are not tall enough.
+
+def userNameAndHeight ():
+     userName=kahliNixon('User name is kahli')
+     userHeight=5.5(User is tall enough to get one the ride )
+if:
+    userName=kahli nixon
+    print("user's name is kahli nixon")
+else:
+    userHight=5.5
+    print("user height is 5.5")
